8b886d6ea46045d80a617d3bc3c2bebbb71b2db2
btbw69 <69147559+btbw69@users.noreply.github.com>
2025-04-24 03:21:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -225,8 +225,6 @@ if uploaded_file:
         technos = df_filtered['Technologie'].dropna().unique()
         techno_choice = st.selectbox("Choisissez une technologie", options=list(technos), key="techno_choice_proginov")
 
-        engagement = st.slider("Durée d'engagement (mois)", min_value=12, max_value=60, step=12, value=36, key="engagement_proginov")
-
         filtered_df_for_debit = df_filtered[df_filtered['Technologie'] == techno_choice]
 
         debits = sorted(filtered_df_for_debit['Débit'].dropna().unique())
